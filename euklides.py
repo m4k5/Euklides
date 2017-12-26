6d5f181222a88c9d514a0171b749a88be11d7650
Maksymilian Skica <maks@riseup.net>
2017-12-26 19:31:19 +0100

    Update euklides.py
    cleanup

--- euklides.py
+++ euklides.py
@@ -7,30 +7,19 @@ Algorytm Euklidesa
                                    |_|   |__/ 
 Z dedykacja dla Pani Profesor Stajno :)
 """
-# Start
-liczba_a = int(raw_input("Podaj liczbe a: "))
-liczba_b = int(raw_input("Podaj liczbe b: "))
-a = liczba_a
-b = liczba_b
+a = int(raw_input("Podaj liczbe a: "))
+b = int(raw_input("Podaj liczbe b: "))
 c = 0
 licznik = 1
-
-# Check czy b juz na poczatku nie zostalo zadeklarowane jako zero
 if b == 0:
-	print("b wynosi 0. Koniec liczenia. NWD wynosi: " + str(a))
+	print("b == 0. Koniec liczenia. NWD wynosi: " + str(a))
 	exit()
-	
-# Liczenie
-# ( <> w Pythonie znaczy "nie rowna sie". Zamiennie != )
-while b<>0:
-	print("\n\n\n")
-	print("Runda " + str(licznik))
+while b != 0:
+	print("\n\n\nRunda " + str(licznik))
 	c = a % b
 	a = b
 	b = c
 	print("a = " + str(a))
 	print("c = " + str(c))
 	print("b = " + str(b))
-	licznik = licznik + 1
-print("OK, wiec NWD(" + str(liczba_a) + ";" + str(liczba_b) + ") = " + str(a))
-zeby_program_sie_nie_zamykal_na_Windowsie = raw_input("Koniec. Nacisnij Enter, aby zamknac...")
+	licznik += 1
